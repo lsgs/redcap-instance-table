fdd59fa67a950d55d2f769909130863adff2c149
Govind Soni <112779983+govindsoniox@users.noreply.github.com>
2025-05-08 15:06:18 +0100

    Update InstanceTable.php
    removed commented out code, and comments on order from previous hardcoded order commit

--- InstanceTable.php
+++ InstanceTable.php
@@ -697,12 +697,8 @@ var <?php echo self::MODULE_VARNAME;?> = (function(window, document, $, app_path
                     .append(taggedField.markup);
             switch(taggedField.page_size) {
                 case 0:
-                    // lengthVal = [10, 25, 50, 100, -1];
-                    // lengthLbl = [10, 25, 50, 100, "<?=$lang['docs_44']?>"]; // "ALL"
                     lengthVal = [-1, 10, 25, 50, 100];
                     lengthLbl = ["<?=$lang['docs_44']?>", 10, 25, 50, 100]; // "ALL"
-
-
                     lengthChange = true;
                     break;
                 case -1:
